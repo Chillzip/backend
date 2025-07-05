805b7d152c90358b7782d490cb35689e6124b637
Chillzip <eighty.bossy_38@icloud.com>
2025-07-05 06:49:28 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -37,6 +37,7 @@ CORS(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
+with app.app_context():
+    db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
