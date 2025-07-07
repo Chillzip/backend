ac16928db6a1df219e909f7c44ebebcb3477ff79
Shaun Cole <shaun@Shauns-MacBook-Pro.local>
2025-07-07 14:42:14 +0100

    Fix wsgi and structure for Render

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
-import sys
-import os
+from src import create_app
 
-# Add the 'src' directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
-
-from main import app  # now importing from src/main.py
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
