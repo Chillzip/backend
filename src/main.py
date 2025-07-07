6a90d87aad24aa38c9550bf5c8667a7242256f0f
Shaun Cole <shaun@Shauns-MacBook-Pro.local>
2025-07-07 15:29:12 +0100

    Add missing main.py file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from src import create_app
-app = create_app()
+from flask import Flask
+from src.routes.test import test_bp
+from src.routes.user import user_bp
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+# Register Blueprints
+app.register_blueprint(test_bp, url_prefix="/test")
+app.register_blueprint(user_bp, url_prefix="/user")
+
+if __name__ == "__main__":
     app.run(debug=True)
